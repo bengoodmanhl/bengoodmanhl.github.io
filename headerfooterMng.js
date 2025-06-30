569e86a98a86b6eea44d01f4c65e013a2ce3b0bb
bengoodmanhl <95678236+bengoodmanhl@users.noreply.github.com>
2025-06-30 09:55:04 +0100

    Create headerfooterMng.js

---
+++ headerfooterMng.js
@@ -0,0 +1,26 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallbak() {
+      this.innerHTML ='
+        <p style "display: flex">
+          <a href = "index.html">Home</a>
+          <a href = "page1.html">Page1</a>
+          <a href = "page2.html">Page2</a>
+        </p>
+      '
+    }
+}
+
+class SpeacialFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML='
+      <p>
+      </p>
+    '
+  }
+}
+
+customElements.define('special-header',SpecialHeader) 
+
+customElements.define('special-footer',SpecialFooter)
+          
+         
